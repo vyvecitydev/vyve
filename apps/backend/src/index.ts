52e9ae18644956fddea4efa796a77f6f1455ad33
Serkan Aysan <serkanaysann@gmail.com>
2026-01-06 17:43:34 +0300

    Update index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -14,6 +14,9 @@ connectDB()
 
 app.use(cors())
 app.use(express.json())
+// JSON limitini artır (ör: 50mb)
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
 
 app.use('/api', routes)
